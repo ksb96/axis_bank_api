769eca2ba96049cf4bfb8dc5db3317b19432e7d4
ksb96 <52975077+ksb96@users.noreply.github.com>
2024-08-19 22:44:47 +0530

    updated init

---
+++ src/main/java/com/bank/axis_bank/AxisBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.bank.axis_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxisBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/axis_bank/AxisBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.axis_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxisBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
